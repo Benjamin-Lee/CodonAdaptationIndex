1b366bde9d8cdd0a3619e8d72fdd37bf6a2a5083
Benjamin Lee <benjamindlee@me.com>
2017-08-16 01:25:42 -0700

    fixed typo

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name = 'CAI',
     packages = ["CAI"],
-    version = '0.1',
+    version = '0.1.1',
     description = 'Python implementation of codon adaptation index',
     author = 'Benjamin Lee',
     author_email = 'benjamin_lee@college.harvard.edu',
     url = 'https://github.com/Benjamin-Lee/CodonAdaptationIndex', # use the URL to the github repo
     classifiers = ["Intended Audience :: Science/Research",
-                   "Topic :: Scientific/Engineering :: Bio-Informatics"
+                   "Topic :: Scientific/Engineering :: Bio-Informatics",
                    "Programming Language :: Python"],
     install_requires=['scipy'],
     license="MIT",
