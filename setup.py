5427a82a51c09d9f23845ba699f91f054c033008
BjornFJohansson <bjornjobb@gmail.com>
2021-11-25 14:50:58 +0000

    added gh tests

--- setup.py
+++ setup.py
@@ -1,30 +1,39 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""docstring."""
+
 from setuptools import setup
+from setuptools import find_packages
 from os import path
+import re
 
 # Get the long description from the README file
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
-setup(
-    name="CAI",
-    packages=["CAI"],
-    version="1.0.3",
-    description="Python implementation of codon adaptation index",
-    long_description=long_description,
-    author="Benjamin Lee",
-    author_email="benjamin_lee@college.harvard.edu",
-    url="https://github.com/Benjamin-Lee/CodonAdaptationIndex",  # use the URL to the github repo
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "Programming Language :: Python",
-    ],
-    install_requires=["scipy", "biopython", "click>=7"],
-    tests_require=["pytest"],
-    setup_requires=["pytest-runner"],
-    license="MIT",
-    # use_2to3=True,
-    python_requires=">=3.4",
-    entry_points={"console_scripts": ["CAI=CAI.cli:cli"]},
-)
+install_requires = []
+with open("requirements.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        install_requires.append(re.split(r"(<|>|=)=", line)[0])
+
+setup(name="CAI",
+      version="1.0.3",
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      description="Python implementation of codon adaptation index",
+      long_description=long_description,
+      author="Benjamin Lee",
+      author_email="benjamin_lee@college.harvard.edu",
+      url="https://github.com/Benjamin-Lee/CodonAdaptationIndex",
+      install_requires=install_requires,
+      tests_require=["pytest"],
+      setup_requires=["pytest-runner"],
+      license="MIT",
+      # use_2to3=True,
+      python_requires=">=3.7",
+      entry_points={"console_scripts": ["CAI=CAI.cli:cli"]},
+      classifiers=["Intended Audience :: Science/Research",
+                   "Topic :: Scientific/Engineering :: Bio-Informatics",
+                   "Programming Language :: Python", ], )
